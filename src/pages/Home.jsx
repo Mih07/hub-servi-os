31dc61b1b851f516d7169ca9cdc9e97abcec1ec6
Michele Santo <micheleespsanto@gmail.com>
2026-08-02 10:53:48 -0300

    atualização categoria

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -258,7 +258,7 @@ export default function Home() {
                           </button>
   
                           <ul className="dropdown-menu shadow-lg border-0 w-100 mt-2" aria-labelledby="dropdownMenuButton" style={{ borderRadius: '12px', padding: '10px' }}>
-                            {['Tudo', 'Academia', 'Artesanato', 'Comida', 'Doces', 'Esmalteria', 'Estética', 'Pet', 'Salão de Beleza',  'Saúde', 'Soluções Digitais'].map((cat) => (
+                            {['Tudo', 'Academia', 'Artesanato', 'Comida', 'Doces', 'Esmalteria', 'Estética', 'Jurídico', 'Pet', 'Salão de Beleza',  'Saúde', 'Soluções Digitais'].map((cat) => (
                               <li key={cat}>
                                 <button 
                                   className="dropdown-item py-3 px-3" 
